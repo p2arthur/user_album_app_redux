19e46c29f03f0906c5a734627a42fc46e3bfaf10
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-05 11:53:49 -0300

    setup: hooked the store to the provider in the root index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,15 @@ import "./assets/index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const rootElement = document.querySelector("#root");
 
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);

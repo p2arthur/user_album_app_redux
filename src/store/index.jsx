400cd7580ec5b59e441c7008a3a9710c0c74235d
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-05 11:48:27 -0300

    feat: setup store

---
+++ src/store/index.jsx
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { usersReducer } from "./slices/usersSlice";
+
+export const store = configureStore({ reducer: { users: usersReducer } });

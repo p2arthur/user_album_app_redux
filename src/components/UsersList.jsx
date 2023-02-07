a7c15c8be9c89fe329b7eefe35cf41cdfd709f70
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-07 17:42:47 -0300

    testing: add testing to see if our fetch user thunk is working properly

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,15 +1,32 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 function UsersList() {
+  //dispatch to dispatch action from our async thunk
   const dispatch = useDispatch();
 
+  //useSelector to get access to ou state.users from the store
+  const { data, isLoading, error } = useSelector((state) => state.users); // {data: [], isLoading: false, error: null}
+
+  //Use effect to dispatch the fetchUsers async thunk
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
 
-  return <div>UsersList</div>;
+  //Testing if the state from ou selector is working
+  //Is the data being fetched?
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  //Has the data failed to fetch?
+  if (error) {
+    return <div>Error loading data...</div>;
+  }
+
+  //Has the data been fetched succesfully?
+  return <div>{data.length} users loaded</div>;
 }
 
 export default UsersList;

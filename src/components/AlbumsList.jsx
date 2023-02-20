c8b6a9e2e4b2c47b194f56f9d63031d7c41cdaf8
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-20 16:24:25 -0300

    feat: implement the display of the albums of a user

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,11 +1,36 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-  console.log(data, error, isLoading);
+  console.log(data);
 
-  return <div>Albums by: {user.name}</div>;
+  let content;
+
+  if (isLoading) {
+    content = <Skeleton times={3} className="w-full h-10" />;
+  } else if (error) {
+    content = <div>Error getting albums</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel header={header}>
+          <div>Photo</div>
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums by: {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;

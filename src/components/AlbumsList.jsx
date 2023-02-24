32f93ea5d29f54013c9e82716055b5fd26171e14
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-24 15:48:57 -0300

    feat: implement the delete album functionality on all components

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,12 +1,18 @@
-import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} from "../store";
 import Skeleton from "./Skeleton";
-import ExpandablePanel from "./ExpandablePanel";
-import Button from "./Button";
 import { faker } from "@faker-js/faker";
+import Button from "./Button";
+
+import AlbumsListItem from "./AlbumsListItem";
 
 function AlbumsList({ user }) {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
-  const [addAlbum, result] = useAddAlbumMutation();
+  console.log(user);
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
+  const [addAlbum, addAlbumsResults] = useAddAlbumMutation();
 
   const handleAddAlbum = () => {
     addAlbum({ title: faker.commerce.productName(), userId: user.id });
@@ -14,18 +20,13 @@ function AlbumsList({ user }) {
 
   let content;
 
-  if (isLoading) {
+  if (isFetching) {
     content = <Skeleton times={3} className="w-full h-10" />;
   } else if (error) {
     content = <div>Error getting albums</div>;
   } else {
     content = data.map((album) => {
-      const header = <div>{album.title}</div>;
-      return (
-        <ExpandablePanel key={album.id} header={header}>
-          <div>Photo</div>
-        </ExpandablePanel>
-      );
+      return <AlbumsListItem key={album.id} album={album} />;
     });
   }
 
@@ -33,7 +34,7 @@ function AlbumsList({ user }) {
     <div>
       <div className="flex justify-between mb-5 items-center">
         <h3 className="text-lg font-bold"> Albums by: {user.name}</h3>
-        <Button loading={result.isLoading} onClick={handleAddAlbum}>
+        <Button loading={addAlbumsResults.isLoading} onClick={handleAddAlbum}>
           + Add new album
         </Button>
       </div>
